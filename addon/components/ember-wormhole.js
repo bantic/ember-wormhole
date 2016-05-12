f3c7169a318b9f838bbddbe92d5ccb97b79f394e
Cory Forsyth <cory.forsyth@gmail.com>
2016-05-12 11:52:20 -0400

    Use `willRender` + `afterRender` + `this.renderer` to work in fastboot
    Proof-of-concept of ember-wormhole working in fastboot
    
    Fastboot doesn't call `didInsertElement`, so use `willRender` to
    schedule into the `afterRender` queue to do the append work.
    
    `this.element` is never assigned in Fastboot, so read it off of
    `this._renderNode.firstNode` instead.
    
    `document` doesn't exist, and SimpleDOM doesn't implement
    `getElementById`, so add a `findElementById` helper function to walk
    SimpleDOM's body and find the destination element by its id.

--- addon/components/ember-wormhole.js
+++ addon/components/ember-wormhole.js
@@ -4,15 +4,53 @@ var computed = Ember.computed;
 var observer = Ember.observer;
 var run = Ember.run;
 
+// walk SimpleDom's body to find the element by id
+function findElementById(body, id) {
+  let node = body.firstChild;
+  while (node) {
+    if (node.getAttribute && node.getAttribute('id') === id) {
+      return node;
+    }
+    if (node.firstChild) {
+      node = node.firstChild;
+    } else if (node.nextSibling) {
+      node = node.nextSibling;
+    } else if (node.parentNode) {
+      node = node.parentNode;
+    } else {
+      break;
+    }
+  }
+}
+
 export default Ember.Component.extend({
   to: computed.alias('destinationElementId'),
   destinationElementId: null,
   destinationElement: computed('destinationElementId', 'renderInPlace', function() {
-    return this.get('renderInPlace') ? this.element : document.getElementById(this.get('destinationElementId'));
+    let id = this.get('destinationElementId');
+    let dom = this.renderer._dom.document.body;
+    let el = findElementById(dom, id);
+    if (!el) {
+      throw new Error(`Unable to find element by id ${id}`);
+    }
+    return el;
   }),
   renderInPlace: false,
 
+  willRender: function() {
+    run.schedule('afterRender', () => {
+      let element = this._renderNode.firstNode;
+      let elementId = element.getAttribute('id');
+      Ember.assert(`Rendered element id does not match expected (el id: ${elementId}, expected ${this.elementId}`, elementId === this.elementId);
+
+      this._firstNode = element.firstChild;
+      this._lastNode = element.lastChild;
+      this.appendToDestination();
+    });
+  },
+
   didInsertElement: function() {
+    // not called when in Fastboot
     this._super(...arguments);
     this._firstNode = this.element.firstChild;
     this._lastNode = this.element.lastChild;
@@ -37,7 +75,8 @@ export default Ember.Component.extend({
 
   appendToDestination: function() {
     var destinationElement = this.get('destinationElement');
-    var currentActiveElement = document.activeElement;
+    // ignore activeElement for this POC
+    // var currentActiveElement = document.activeElement;
     if (!destinationElement) {
       var destinationElementId = this.get('destinationElementId');
       if (destinationElementId) {
@@ -47,9 +86,12 @@ export default Ember.Component.extend({
     }
 
     this.appendRange(destinationElement, this._firstNode, this._lastNode);
+    // ignore activeElement for this POC
+    /*
     if (document.activeElement !== currentActiveElement) {
       currentActiveElement.focus();
     }
+    */
   },
 
   appendRange: function(destinationElement, firstNode, lastNode) {
